c8230d0b835064949307fad1296ec306d4e3692e
David Bradshaw <dave.bradshaw@smartpension.co.ukl>
2020-06-19 01:17:00 +0100

    Disable rules that require tsconfig

--- rules/airbnb-typescript.js
+++ rules/airbnb-typescript.js
@@ -9,10 +9,34 @@ console.log(configs.map((config) => `  eslint-config-${config}`).join('\n'))
 module.exports = {
   overrides: [
     {
-      files: ['*.ts{s}', '**/*.ts{s}'],
+      files: ['*.ts', '**/*.ts', '*.tsx', '**/*.tsx'],
       extends: configs,
       rules: {
         '@typescript-eslint/naming-convention': 0, // conflicts with unicorn
+        'no-use-before-define': 0,
+        'import/extensions': 0,
+        'mport/no-unresolved': 0,
+        'react/jsx-filename-extension': 0,
+
+        // Disable rules that need tsconfig
+        '@typescript-eslint/dot-notation': 0,
+        '@typescript-eslint/no-throw-literal' : 0,
+
+        '@typescript-eslint/await-thenable': 0,
+        '@typescript-eslint/no-floating-promises': 0,
+        '@typescript-eslint/no-for-in-array': 0,
+        '@typescript-eslint/no-implied-eval': 0,
+        '@typescript-eslint/no-misused-promises': 0,
+        '@typescript-eslint/no-unnecessary-type-assertion': 0,
+        '@typescript-eslint/no-unsafe-assignment': 0,
+        '@typescript-eslint/no-unsafe-call': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-unsafe-return': 0,
+        '@typescript-eslint/prefer-regexp-exec': 0,
+        '@typescript-eslint/require-await': 0,
+        '@typescript-eslint/restrict-plus-operands': 0,
+        '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/unbound-method': 0,
       },
     },
   ],
