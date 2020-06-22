98df7d2c0bab0f3cfa430f6b537f618b349c6c5e
David Bradshaw <dave.bradshaw@smartpension.co.ukl>
2020-06-22 13:53:44 +0100

    Improve support for CRA

--- plugins.js
+++ plugins.js
@@ -1,18 +1,19 @@
 const { cosmiconfigSync } = require('cosmiconfig')
 
-const { hasAnyDep } = require('./lib/utils')
+const isModuleAvailable = require('./lib/is-module-available')
 
 const explorerSync = cosmiconfigSync('eslint')
 const eslintrc = explorerSync.search()
-const { env = {} } = eslintrc.config
+const { env: environment = {} } = eslintrc.config
 
-const hasBabel = hasAnyDep('babel') || hasAnyDep('@babel/core')
+const hasBabel = isModuleAvailable('babel') || isModuleAvailable('@babel/core')
 
 const plugins = []
 
 if (hasBabel) plugins.push('babel')
-if (env.node) plugins.push('node')
+if (environment.node) plugins.push('node')
 
-if (env.browser && !hasBabel && !hasAnyDep('typescript')) plugins.push('compat')
+if (environment.browser && !hasBabel && !isModuleAvailable('typescript'))
+  plugins.push('compat')
 
 module.exports = plugins
